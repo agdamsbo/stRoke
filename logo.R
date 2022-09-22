3bea2790b5b8d2d630d70b5caaff1e8bff99b3c8
AG Damsbo <agdamsbo@pm.me>
2022-09-22 15:14:29 +0200

    hex logo

---
+++ logo.R
@@ -0,0 +1,9 @@
+# Logo creation
+
+logo_path <- "/Users/au301842/Downloads/Logoer/DSC-logo.png"
+
+hexSticker::sticker(logo_path, package="stRoke", 
+                    p_size=22, s_x=1, s_y=.7, s_width=.6,
+                    h_fill = "coral", # Fill color
+                    h_color = "maroon", # Border color
+                    filename="hexlogo.png")
