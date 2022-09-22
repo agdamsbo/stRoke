86e04ad78e3c370caa0fc93b2aeae3b10467e77d
AG Damsbo <agdamsbo@pm.me>
2022-09-22 15:22:09 +0200

    logo and all

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 [![R-CMD-check](https://github.com/agdamsbo/stRoke/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/agdamsbo/stRoke/actions/workflows/R-CMD-check.yaml)
 <!-- badges: end -->
 
-# stRoke
+# stRoke package <img src="man/figures/hexlogo.png" align="right" />
 
 This package contains tools used for my (Andreas) research work and for the ENIGMA project.    
 Please use the package or grab bits of the functions. Mind the licensing.
--- logo.R
+++ logo.R
@@ -3,7 +3,8 @@
 logo_path <- "/Users/au301842/Downloads/Logoer/DSC-logo.png"
 
 hexSticker::sticker(logo_path, package="stRoke", 
-                    p_size=22, s_x=1, s_y=.7, s_width=.6,
+                    p_size=10, s_x=1, s_y=.7, s_width=.6,
                     h_fill = "coral", # Fill color
                     h_color = "maroon", # Border color
-                    filename="hexlogo.png")
+                    dpi = 100,
+                    filename="man/figures/hexlogo.png")
