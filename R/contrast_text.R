0e15b425d75b961a914dc60882218b481d82b0ef
AG Damsbo <agdamsbo@pm.me>
2023-04-12 19:55:40 +0200

    gp

--- R/contrast_text.R
+++ R/contrast_text.R
@@ -1,9 +1,11 @@
 
 
 #' @title Contrast Text Color
-#' @description Calculates the best contrast text color for a given background color.
+#' @description Calculates the best contrast text color for a given 
+#' background color.
 #' @param background A hex/named color value that represents the background.
-#' @param light_text A hex/named color value that represents the light text color.
+#' @param light_text A hex/named color value that represents the light text 
+#' color.
 #' @param dark_text A hex/named color value that represents the dark text color.
 #' @param threshold A numeric value between 0 and 1 that is used to determine 
 #' the luminance threshold of the background color for text color.
@@ -15,7 +17,8 @@
 #' The function is based on the example provided by teppo: 
 #' https://stackoverflow.com/a/66669838/21019325.
 #' The different methods provided are based on the methods outlined in the 
-#' StackOverflow thread: https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
+#' StackOverflow thread: 
+#' https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
 #' @return A character string that contains the best contrast text color.
 #' @examples
 #' contrast_text(c("#F2F2F2", "blue"))
@@ -26,24 +29,24 @@
 #' @importFrom grDevices col2rgb
 #'
 contrast_text <- function(background,
-                            light_text = 'white',
-                            dark_text = 'black',
-                            threshold = 0.5,
-                            method = "perceived_2") {
+                          light_text = 'white',
+                          dark_text = 'black',
+                          threshold = 0.5,
+                          method = "perceived_2") {
   if (method == "relative") {
-    luminance <- c(c(.2126, .7152, .0722) %*% grDevices::col2rgb(background) / 255)
+    luminance <-
+      c(c(.2126, .7152, .0722) %*% grDevices::col2rgb(background) / 255)
   } else if (method == "perceived") {
-    luminance <- c(c(.299, .587, .114) %*% grDevices::col2rgb(background) / 255)
+    luminance <-
+      c(c(.299, .587, .114) %*% grDevices::col2rgb(background) / 255)
   } else if (method == "perceived_2") {
     luminance <- c(sqrt(colSums((
       c(.299, .587, .114) * grDevices::col2rgb(background)
     ) ^ 2)) / 255)
   }
   
-  ifelse(
-    luminance < threshold,
-    light_text,
-    dark_text
-  )
+  ifelse(luminance < threshold,
+         light_text,
+         dark_text)
 }
 
--- tests/testthat/test-contrast_text.R
+++ tests/testthat/test-contrast_text.R
@@ -5,6 +5,8 @@ library(testthat)
 test_that("contrast_text() returns the correct text color", {
   expect_equal(contrast_text("#FFFFFF"), "black")
   expect_equal(contrast_text("#000000"), "white")
-  expect_equal(contrast_text("#FFFFFF", light_text="blue", dark_text="green"), "green")
-  expect_equal(contrast_text("#000000", light_text="blue", dark_text="green"), "blue")
+  expect_equal(contrast_text("#FFFFFF", light_text="blue", dark_text="green"), 
+               "green")
+  expect_equal(contrast_text("#000000", light_text="blue", dark_text="green"), 
+               "blue")
 })
