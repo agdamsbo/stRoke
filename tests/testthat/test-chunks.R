163e9708657222c5902d7541d5831eb34b210b36
AG Damsbo <andreas@gdamsbo.dk>
2023-09-05 10:56:43 -0700

    Modified tests to avoid expected GHA test fail with sprintf()

--- tests/testthat/test-chunks.R
+++ tests/testthat/test-chunks.R
@@ -1,26 +1,27 @@
-# library(testthat)
+library(testthat)
 test_that("chunks_of_n returns correct", {
   expect_length(chunks_of_n(seq_len(100), 7),15)
   
   expect_equal(lengths(chunks_of_n(seq_len(30), 7, even = TRUE), 
                        use.names = FALSE), c(6, 6, 6, 6, 6))
   
-  ds <- data.frame(nm = paste0("Sub",
-                               sprintf("%03s", rownames(stRoke::talos))), 
+  # This is the example from the function, but I believe it fails in GitHub testing
+  # ds <- data.frame(nm = paste0("Sub",
+  #                              sprintf("%03s", rownames(stRoke::talos))),
+  #                  stRoke::talos)
+  
+  ds <- data.frame(nm = paste0("Sub",rownames(stRoke::talos)), 
                    stRoke::talos)
   
-  is.list(ds)
-  
-  class(list(ds))
-  
   expect_equal(head(names(chunks_of_n(ds, 7, 
-                                      pattern = "Sub[0-9]{3}", label = "grp")),
-                    1),"grp-Sub038-Sub011")
+                                      pattern = "Sub([0-9]+)", label = "grp")),
+                    1),"grp-Sub38-Sub11")
   
   expect_equal(
-    ds[order(ds$nm),] |> chunks_of_n(7, pattern = "Sub[0-9]{3}", label =
-                                       "grp") |>  head(1) |> names(),
-    "grp-Sub001-Sub020"
+    ds[order(as.numeric(rownames(stRoke::talos))), ] |> 
+      chunks_of_n(7, pattern = "Sub([0-9]+)", label = "grp") |>  
+      head(1) |> names(),
+    "grp-Sub1-Sub20"
   )
   
   ## Errors
@@ -35,22 +36,27 @@ test_that("n_chunks returns correct", {
   expect_equal(lengths(n_chunks(seq_len(30), 7, even = TRUE), 
                        use.names = FALSE), rep(5,6))
   
-  ds <- data.frame(nm = paste0("Sub",
-                               sprintf("%03s", rownames(stRoke::talos))), 
+  # This is the example from the function, but I believe it fails in GitHub testing
+  # ds <- data.frame(nm = paste0("Sub",
+  #                              sprintf("%03s", rownames(stRoke::talos))),
+  #                  stRoke::talos)
+  
+  ds <- data.frame(nm = paste0("Sub",rownames(stRoke::talos)), 
                    stRoke::talos)
   
   expect_equal(head(names(n_chunks(ds, 7, 
-                                      pattern = "Sub[0-9]{3}", label = "grp")),
-                    1),"grp-Sub038-Sub603")
+                                      pattern = "Sub([0-9]+)", label = "grp")),
+                    1),"grp-Sub38-Sub603")
   
   expect_equal(
-    ds[order(ds$nm),] |> n_chunks(7, pattern = "Sub[0-9]{3}", label =
-                                       "grp") |>  head(1) |> names(),
-    "grp-Sub001-Sub072"
+    ds[order(as.numeric(rownames(stRoke::talos))), ] |> 
+      n_chunks(7, pattern = "Sub([0-9]+)", label = "grp") |>  
+      head(1) |> names(),
+    "grp-Sub1-Sub72"
   )
   
   ## Errors
-  expect_error(n_chunks(list(ds), 7, pattern = "Sub[0-9]{3}", label = "grp"))
+  expect_error(n_chunks(list(ds), 7, pattern = "Sub([0-9]+)", label = "grp"))
   
   
 })
